a654c83bc44c33cac8ed27f24503aecbe69fb182
kazuma1989 <kazuma.ebina@quipper.com>
2021-04-04 23:31:40 +0900

    chore: TS で書けるようにした

---
+++ build.js
@@ -0,0 +1,45 @@
+// @ts-check
+/// <reference lib="es2018" />
+
+const chokidar = require("chokidar")
+const esbuild = require("esbuild")
+const path = require("path")
+
+async function run() {
+  const [watch] = process.argv.slice(2)
+
+  try {
+    const entryPoint = path.resolve(__dirname, "src/index.ts")
+    const outfile = path.resolve(__dirname, "dist/index.js")
+
+    const buildSync = () => {
+      esbuild.buildSync({
+        charset: "utf8",
+        bundle: false,
+        platform: "node",
+        target: "node10",
+        format: "cjs",
+        entryPoints: [entryPoint],
+        outfile,
+        define: {
+          // Suppress warnings from Formidable.
+          "global.GENTLY": JSON.stringify(false),
+        },
+      })
+    }
+
+    if (watch) {
+      chokidar
+        .watch(path.join(__dirname, "src/**/*.{ts,tsx}"))
+        .on("all", buildSync)
+    } else {
+      buildSync()
+    }
+  } catch (err) {
+    console.error(err)
+
+    process.exit(1)
+  }
+}
+
+run()
---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import express from "express"
+import formidable from "formidable"
+import * as path from "path"
+
+const app = express()
+const port = 5000
+
+// 開発サーバーなので緩い制約で CORS を受け入れる。
+app.use((req, resp, next) => {
+  resp.setHeader("Access-Control-Allow-Origin", "*")
+  resp.setHeader("Access-Control-Allow-Methods", "*")
+
+  next()
+})
+
+const storageDir = path.join(__dirname, "storage")
+
+// ファイルアップロードのエンドポイント。
+app.post("/storage", (req, resp, next) => {
+  const form = new formidable.IncomingForm({
+    uploadDir: storageDir,
+    keepExtensions: true,
+  })
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err)
+      return
+    }
+
+    const [file] = Object.values(files)
+    if (!file || Array.isArray(file)) {
+      next()
+      return
+    }
+
+    const host = req.headers.host ?? `localhost:${port}`
+
+    resp.json({
+      downloadURL: `http://${host}/storage/${path.basename(file.path)}`,
+    })
+  })
+})
+
+// storage ディレクトリの中身は静的ファイルとしてレスポンスする。
+app.use("/storage", express.static(storageDir))
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`)
+})
