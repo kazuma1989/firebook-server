317dac7e4156764b558c89d32b891ba4d1e25d65
kazuma1989 <kazuma.ebina@quipper.com>
2021-04-07 21:36:01 +0900

    feat: ログをシンプルにした

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ async function run() {
 
     // ロギング
     server.on("request", (req, resp) => {
-      console.log("request", req.method, req.url)
+      console.log(req.method, req.url)
 
       req.on("warn", (info) => {
         switch (info.type) {
